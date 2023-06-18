4feee4bc919f59f1cf1b88fdcc62d9071dcd13d5
dmitriy-kochetkov <dmi3y.kochetkov@gmail.com>
2023-06-18 17:47:22 +0300

    update Chat page

--- src/core/Router/Route.ts
+++ src/core/Router/Route.ts
@@ -67,18 +67,6 @@ export default class Route {
     }
 
     render() {
-        // if (this.checkAuth()) {
-        //     if (!this._block) {
-        //         // @ts-ignore
-        //         this._block = new this._blockClass({...this._componentProps, routerParams: this.getParams()});
-        //     } else {
-        //         this._block.setProps({...this._componentProps, routerParams: this.getParams()});
-        //     }
-        //     renderDOM(this._block, this._props.rootQuery);
-        // } else {
-        //     this._redirect()
-        // }
-
         if (this.checkAuth()) {
             // @ts-ignore
             this._block = new this._blockClass({...this._componentProps, routerParams: this.getParams()});
