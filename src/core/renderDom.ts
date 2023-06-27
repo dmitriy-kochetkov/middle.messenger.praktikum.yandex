5b30c8e63138c85d6390118c7aa406ed2716fb2b
dmitriy-kochetkov <dmi3y.kochetkov@gmail.com>
2023-06-27 22:54:10 +0300

    fix netlify casesensetive filenaming

--- src/core/renderDom.ts
+++
@@ -1,8 +0,0 @@
-import Block from './Block';
-
-export default function renderDOM(block: Block, selector: string = '#app') {
-    const root = document.querySelector(selector);
-
-    root!.innerHTML = '';
-    root!.appendChild(block.getContent());
-}
